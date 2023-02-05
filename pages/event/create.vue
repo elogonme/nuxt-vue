aaa287195d015bca536f84e1085ee7b13a3e1223
Eldar Humbatov <elogon@gmail.com>
2023-02-05 18:04:01 -0500

    create dynamic pages and error page

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Creat and event',
+      meta: [
+        {
+          hid: 'discription',
+          name: 'descrition',
+          content: 'You can create a new event in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
