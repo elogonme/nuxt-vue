adab9e4e366b675c3fde49ee4175685b3773b608
Eldar Humbatov <elogon@gmail.com>
2023-02-05 18:20:05 -0500

    change to await

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
+  loading: { color: '#39b982' },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
